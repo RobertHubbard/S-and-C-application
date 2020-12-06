7c403d6ec86594b7663137ea4d71541a1eed4739
RobertHubbard <Rob.hubbard0815@gmail.com>
2020-12-06 16:52:58 -0600

    Initial Commit

---
+++ S and C application/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  S and C application
+//
+//  Created by Robert Hubbard on 12/6/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
